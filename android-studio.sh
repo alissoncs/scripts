ec1fffe64fcc404c720ebd6156a6e377e9d776ea
Alisson Carvalho <webalissoncs@gmail.com>
2016-02-20 09:17:26 -0200

    fix bug installation android studio

--- android-studio.sh
+++ android-studio.sh
@@ -6,6 +6,8 @@ wget https://dl.google.com/dl/android/studio/ide-zips/1.5.1.0/android-studio-ide
 rm -rf /tmp/android-studio-ide-*-linux.zip
 sudo mv /tmp/android-studio /opt/android-studio
 
+sudo apt-get install -ydd lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+
 sudo mkdir /opt/android-sdk
 sudo chown alisson:alisson /opt/android-sdk
 
