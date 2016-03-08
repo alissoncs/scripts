9fe3ccbcacc9d1a0d28972e3d9850af341beb17a
Alisson Carvalho <webalissoncs@gmail.com>
2016-03-08 08:48:38 -0300

    added variable $USER to android studio installation script

--- android-studio.sh
+++ android-studio.sh
@@ -9,7 +9,7 @@ sudo mv /tmp/android-studio /opt/android-studio
 sudo apt-get install -ydd lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 
 sudo mkdir /opt/android-sdk
-sudo chown alisson:alisson /opt/android-sdk
+sudo chown $USER:$USER /opt/android-sdk
 
 # mv android studio
 echo "export ANDROID_HOME=/opt/android-sdk" >> ~/.bashrc
