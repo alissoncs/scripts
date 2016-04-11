fd088011a99a8aaf396fc2f1e468130fddce3905
Alisson Carvalho <webalissoncs@gmail.com>
2016-04-11 08:52:06 -0300

    Added context variables to Android Studio setup

--- android-studio.sh
+++ android-studio.sh
@@ -2,15 +2,16 @@
 # installation of android studio
 
 URL="https://dl.google.com/dl/android/studio/ide-zips/2.0.0.20/android-studio-ide-143.2739321-linux.zip"
+DEST="/tmp/android-studio.zip"
 
-wget "${URL}" -P /tmp/
-[ -f /tmp/android-studio-ide-*-linux.zip ] && unzip /tmp/android-studio-ide-*-linux.zip
-rm -rf /tmp/android-studio-ide-*-linux.zip
-sudo mv /tmp/android-studio /opt/android-studio
+wget "${URL}" -O "${DEST}"
+[ -f "${DEST}" ] && unzip "${DEST}"
+rm -rf "${DEST}"
+# sudo mv /tmp/android-studio /opt/android-studio
 
 sudo apt-get install -ydd lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 
-sudo mkdir /opt/android-sdk
+# sudo mkdir /opt/android-sdk
 sudo chown $USER:$USER /opt/android-sdk
 
 # mv android studio
